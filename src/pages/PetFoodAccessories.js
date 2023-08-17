cc051d9e6035146a6a8e16bcdf0580f0e7d3033c
unknown <abhilash.rpee18@gmail.com>
2023-08-17 16:58:43 +0530

    dynamic table

--- src/pages/PetFoodAccessories.js
+++ src/pages/PetFoodAccessories.js
@@ -16,8 +16,12 @@ import second from "../resource/Web - Menu/posts_3.png";
 import SearchIcon from "@mui/icons-material/Search";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { DataGrid } from "@mui/x-data-grid";
+import CommonTable from "../components/CommonTable";
 const PetFoodAccessories = () => {
   const [data, setData] = useState([]);
+  const [petData, setPetData] = useState([]);
   useEffect(() => {
     axios
       .get("https://demo.emeetify.com:81/pet/stocks/")
@@ -25,8 +29,28 @@ const PetFoodAccessories = () => {
         setData(response.data.data !== [] ? response.data.data : "")
       )
       .catch((e) => console.log(e));
+    // let i = {};
+    //   for(let i = 0; i<data.length ; i++){
+    //     console.log(data[i],"======>>>>>>");
+    //     setPetData(data[i]);
+    //   }
   }, []);
-  console.log(data);
+
+  
+  console.log(petData,"petData====>");
+const columns =[
+  { key: 'name', label: 'Product Name' },
+  { key: 'stock_id', label: 'Product id' },
+  { key: 'stock_type', label: 'Category' },
+  { key: 'qty_type', label: 'Qty' },
+  { key: 'price', label: 'Price' },
+  { key: 'avail_qty', label: 'Stock Available' },
+  { key:'', label: 'Action' },
+];
+
+
+  const tableHeight = '400px';
+  const apiUrl = "https://demo.emeetify.com:81/pet/stocks/";
   return (
     <>
       <h4 style={{ marginLeft: "50px", fontWeight: "600" }}>
@@ -41,7 +65,7 @@ const PetFoodAccessories = () => {
         <div style={{ position: "relative" }}>
           <img src={second} alt="firstImage" className="secondImg" />
           <span className="imgThreshold">Threshold Reached</span>
-          <span className="imgThresholdCount">32</span>
+          <span className="imgThresholdCount">{data.length}</span>
         </div>
       </Stack>
       <Paper
@@ -59,7 +83,12 @@ const PetFoodAccessories = () => {
       </Paper>
 
       <Card style={{ marginTop: "30px" }}>
-        <TableContainer>
+
+
+      <CommonTable  columns={columns} apiUrl={apiUrl} height={tableHeight}/>
+        
+       
+        {/* <TableContainer>
           <Table>
             <TableHead >
               <TableRow >
@@ -74,19 +103,20 @@ const PetFoodAccessories = () => {
             </TableHead>
             <TableBody >
               {data.map((row) => (
-                <TableRow key={row.name} >
+                <TableRow key={row.name} style={{textAlign:"center"}}>
                   <TableCell >{row.name}</TableCell>
                   <TableCell >{row.stock_id}</TableCell>
                   <TableCell >{row.stock_type}</TableCell>
                   <TableCell >{row.qty_type}</TableCell>
                   <TableCell >{row.price}</TableCell>
                   <TableCell >{row.avail_qty}</TableCell>
-                  <TableCell >{row.status}</TableCell>
+                  <TableCell >
+                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
-        </TableContainer>
+        </TableContainer> */}
       </Card>
     </>
   );
